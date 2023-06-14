2a3ddd2ad68eeabe4cdc4fec324f9613310990c3
elilionel7 <elilionel7@gmail.com>
2023-06-14 14:28:11 -0500

    complete refactoring js

--- practice-for-week-PREP-js-refactor-project-main/problems.js
+++ practice-for-week-PREP-js-refactor-project-main/problems.js
@@ -7,8 +7,8 @@
     Write a function `plusTen` that takes in a number as an argument and
     returns the sum of that number and 10.
 */
-function plusFive(num) {
-    return num + 5;
+function plusTen(num) {
+    return num + 10;
 }
 
 
@@ -21,12 +21,14 @@ function plusFive(num) {
     Write a function `returnSevens(max)` that returns an array that contains
     multiples of 7 that are less than max.
 */
-function printFives(max) {
-    for (let i = 0; i < max; i++) {
-        if (i % 5 === 0) {
-            console.log(i);
-        }
+function returnSevens(max) {
+    const arr = [];
+    let i = 0;
+    while (i<max){
+        arr.push(i);
+        i +=7
     }
+    return arr
 }
 
 
@@ -43,8 +45,8 @@ function printFives(max) {
     function should return `true` if *both* `word1` and `word2` are found in
     the sentence, and `false` if neither or only 1 is found.
 */
-function eitherStringIncluded(sentence, word1, word2) {
-    return sentence.includes(word1) || sentence.includes(word2);
+function bothStringsIncluded(sentence, word1, word2) {
+    return sentence.includes(word1) && sentence.includes(word2);
 }
 
 
@@ -58,12 +60,12 @@ function eitherStringIncluded(sentence, word1, word2) {
     returns the product of all the numbers. The product of an array  is the number
     you get when you multiply all the numbers together.
 */
-function sumArray(arr) {
-    let sum = 0;
+function productArray(arr) {
+    let prod = 1;
     for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+        prod *= arr[i];
     }
-    return sum;
+    return prod;
 }
 
 
@@ -76,8 +78,8 @@ function sumArray(arr) {
     Write a function `fiveAndEleven` that takes in a number and returns `true`
     if the number is divisible by BOTH 5 and 11 and `false` otherwise.
 */
-function threeOrSeven(num) {
-    return num % 3 === 0 || num % 7 === 0;
+function fiveAndEleven(num) {
+    return num % 5 === 0 && num % 11 === 0;
 }
 
 
@@ -90,14 +92,16 @@ function threeOrSeven(num) {
     Write a function, `countConsonants(word)`, that takes in a string word and
     returns the number of consonants in the word.
 */
-function countVowels(word) {
-    const vowels = ["a", "e", "i", "o", "u"];
+function countConsonants(word) {
+    word = word.toLowerCase();
+    const vowels = ["a", "e", "i", "o", "u"," "]; // included " " for case where the word includes " " since I will be using length of word
     let count = 0;
     for (let i = 0; i < word.length; i++) {
         if (vowels.includes(word[i])) {
             count++;
         }
     }
+    count = word.length - count;
     return count;
 }
 
@@ -118,18 +122,27 @@ function countVowels(word) {
     `.split('')` function on strings to make a copy of the string as an array.
     The `.join('')` function joins the elements in an array into a string.
 */
-function whisper(str) {
-    return str.toLowerCase();
+function alternatingLetters(str) {
+    let result = "";
+    for (let i = 0; i<str.length; i++){
+        if ((i%2==0) && (str[i]!== " ")){
+            result += str[i].toLowerCase();
+        }else if ((i%2==1) && (str[i]!== " ")) {
+            result += str[i].toUpperCase();
+        }else if (str[i]==" "){
+            result += " ";
+        }
+    }
+    return result;
 }
 
 
 module.exports = {
-    plusFive,
+    plusTen,
     returnSevens,
     bothStringsIncluded,
     productArray,
     fiveAndEleven,
     countConsonants,
-    threeOrSeven,
     alternatingLetters
 }
