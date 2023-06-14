616b0c9c185479c67479a3f7e38fc5d112f1add6
elilionel7 <elilionel7@gmail.com>
2023-06-14 10:42:39 -0500

    solved some questions

--- practice-for-week-PREP-nonstandard-for-loops-main/problems.js
+++ practice-for-week-PREP-nonstandard-for-loops-main/problems.js
@@ -1,16 +1,41 @@
 function oddIndices(arr) {
     // Return an array containing all the odd indices in the array
     // Your code here
+    let newArr = [];
+    let i = 1;
+    while (i<arr.length){
+        newArr.push(arr[i])
+        i+=2
+    }
+    return newArr
 }
 
 function oddReverse(arr) {
     // Return an array containing all the odd indices starting from the end
     // Your code here
+    let newArr = [];
+    let i = arr.length;
+    while (i>=1){
+        if (i%2===1){
+            newArr.push(arr[i])
+            i-=1
+        }
+    }
+    return newArr
+
 }
 
 function secondPower(arr) {
     // Return an array containing all indices that are powers of 2
     // Your code here
+    // powers of 2, 1 2 4 8 16 32. ie current number *2
+    let newArr = [];
+    let   i = 1;
+    while (i < arr.length){
+        newArr.push(arr[i]);
+        i *= 2
+    }
+    return newArr
 }
 
 function nthPower(arr, n) {
