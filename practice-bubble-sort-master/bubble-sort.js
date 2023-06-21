883c461de2bc6e2f2157f093abc284582f5177c3
elilionel7 <elilionel7@gmail.com>
2023-06-21 15:05:41 -0500

    bubble sort completed

--- practice-bubble-sort-master/bubble-sort.js
+++ practice-bubble-sort-master/bubble-sort.js
@@ -2,16 +2,30 @@
 function bubbleSort(arr) {
 
     // Iterate through the array
+    for(let i = 0; i < arr.length; i++){
 
+      for(let j = 0; j < arr.length - 1 - i; j++){
+        let curVal = arr[j];
+        let neighbor = arr[j+1];
+
+        if( curVal > neighbor){
+          arr[j] = neighbor;
+          arr[j+1] = curVal;
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
-        // Do not move this console.log
+        // console.log
         console.log(arr.join(","));
+        }
 
     // If you get to the end of the array and no swaps have occurred, return
+      }
+
 
     // Otherwise, repeat from the beginning
+
+    }
+    return arr
 
 }
 
